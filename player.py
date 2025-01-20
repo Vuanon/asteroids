d0eff4ce023788528a95026ae91f93d0f475e689
Karlson <karlsontay@gmail.com>
2025-01-21 05:06:34 +0800

    Keep shoot timer logic within shoot method

--- player.py
+++ player.py
@@ -28,6 +28,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -37,12 +38,15 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE]:# and self.timer <= 0:
             self.shoot()
 
     def shoot(self):
+        if self.timer > 0: # keeps logic to shoot() method
+            return
         shot = Shot(self.position[0], self.position[1])
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
 
     def spawn(self, radius, position, velocity):
         asteroid = Asteroid(position.x, position.y, radius)
